6e8d7319691db52c322f37a27b479f7d36c25c10
Mehmet ICME <44260512+MehmetIcm@users.noreply.github.com>
2021-03-06 22:42:41 +0300

    Creating DatabaseContext and ConnectionString

---
+++ MyEvernote.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using MyEvernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace MyEvernote.DataAccessLayer
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<EvernoteUser> EvernoteUsers { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Liked> Likes { get; set; }
+    }
+}
