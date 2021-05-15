6e8139b6e0bc07bded7c4d815d882c6ae692848b
Mehmet ICME <44260512+MehmetIcm@users.noreply.github.com>
2021-05-15 16:16:26 +0300

    Creating Category View Pages

--- MyEvernote.Entities/Category.cs
+++ MyEvernote.Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -9,9 +10,9 @@ namespace MyEvernote.Entities
     [Table("Categories")]
     public class Category:MyEntityBase
     {
-        [Required, StringLength(50)]
+        [DisplayName("Başlık"),Required, StringLength(50)]
         public string Title { get; set; }
-        [StringLength(200)]
+        [DisplayName("Açıklama"), StringLength(200)]
         public string Description { get; set; }
         public virtual List<Note> Notes { get; set; } //The property realtional with another class so we defined as virtual 
 
