fe19e1891d6d763d1bb999f0ba00c8332f64d50a
Mehmet ICME <44260512+MehmetIcm@users.noreply.github.com>
2021-05-29 17:41:21 +0300

    Deleting Relational Datas(Code Solution)

--- MyEvernote.BusinessLayer/CategoryManager.cs
+++ MyEvernote.BusinessLayer/CategoryManager.cs
@@ -2,12 +2,38 @@
 using MyEvernote.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyEvernote.BusinessLayer
 {
+    //Deleting Relational Datas
     public class CategoryManager:ManagerBase<Category>
     {
+        public override int Delete(Category category)
+        {
+            NoteManager noteManager = new NoteManager();
+            LikedManager likedManager = new LikedManager();
+            CommentManager commentManager = new CommentManager();
 
+            //Deleting Notes which relational with Category
+            foreach (Note note in category.Notes.ToList())
+            {
+                //Deleting Likes which relational with Notes
+                foreach (Liked like in note.Likes.ToList())
+                {
+                    likedManager.Delete(like);
+                }
+
+                //Deleting comments which relational with Notes
+                foreach (Comment comment in note.Comments.ToList())
+                {
+                    commentManager.Delete(comment);
+                }
+
+                noteManager.Delete(note);
+            }
+            return base.Delete(category);
+        }
     }
 }
---
+++ MyEvernote.BusinessLayer/CommentManager.cs
@@ -0,0 +1,12 @@
+﻿using MyEvernote.BusinessLayer.Abstract;
+using MyEvernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class CommentManager:ManagerBase<Comment>
+    {
+    }
+}
